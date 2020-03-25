0917b6f10a4623b5546f4ae88221d76da7d4e089
Kolawole Abdoulaye <ablo110@live.fr>
2020-03-26 00:03:07 +0100

    adding corpus

---
+++ WordPrediction/Corpus_statistics/README.md
@@ -0,0 +1,15 @@
+# Corpus_statistcs
+Ce programme analyse un fichier texte et calcule le nombre d'occurence de mots dans un texte dans le but le faire le corpus de ce texte.
+Le programme crée un fichier Json qui contient chaque mot et le nombre de fois que celui apparait dans le texte.
+
+C'est une application en ligne de commande appelée avec des arguments.
+
+> app.py [-h] [--out PATH] file
+
+La commande de base est :
+
+Cette commande afficher le corpus sur la sortie standard
+> python app.py file
+
+Cette commande avec la commande optionelle --out sauvegarde le corpus dans un fichier texte.
+> python app.py --out <nom_fichier> file
---
+++ WordPrediction/Corpus_statistics/app.py
@@ -0,0 +1,63 @@
+import argparse
+import json
+import time
+
+
+def inputCorpus(word):
+    if word in corpus:
+        corpus[word] += 1
+    else:
+        corpus[word] = 1
+
+
+def split(listwords):
+    result = []
+    for elem in listwords:
+        x = elem.split(" ")
+        for string in x:
+            w = string.translate({ord(i): None for i in '\n.,?!:;'})  # delete the ponctuations
+            result.append(w)  # add each word in file to a list
+
+    return result
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='corpus text file to analyse')
+    parser.add_argument('-o', '--out', metavar='PATH',
+                        help='path to the file where to output the result of the analysis')
+    args = parser.parse_args()
+
+    corpus = {}
+
+    print('Analysing', args.file)
+
+    book = open('book', "r")
+    sentences = book.readlines()  # list of all sentences in file
+    words = split(sentences)  # list of all words in file
+    for mot in words:
+        inputCorpus(mot)  # put each word in the corpus
+
+    with open('corpus.json', 'w') as json_file:  # result as json file
+        json.dump(corpus, json_file)
+
+    print("Waiting...")
+    time.sleep(2)
+    print("Your Json is created")
+
+    if args.out:  # if optional argument --out
+        time.sleep(0.5)
+        print('Writing output to', args.out)
+
+        with open(args.out + '.txt', 'w') as txt_file:  # result as text file
+            for key, value in corpus.items():
+                txt_file.write(key + ":" + str(value) + "\n")
+
+        txt_file.close()
+    else:  # default option
+        print('Writing output to stdout')
+        for key, value in corpus.items():
+            print(key + ":" + str(value))
+
+    json_file.close()
+    book.close()
