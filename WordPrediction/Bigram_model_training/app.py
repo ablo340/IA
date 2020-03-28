aa82f5b14219a1b8cea6ee099b73ae89117c4098
Kolawole Abdoulaye <ablo110@live.fr>
2020-03-28 01:37:43 +0100

    prediction next word app

---
+++ WordPrediction/Bigram_model_training/app.py
@@ -0,0 +1,54 @@
+from nltk import bigrams, ConditionalFreqDist, FreqDist
+from nltk.lm.preprocessing import flatten, pad_both_ends
+from nltk.util import everygrams
+from nltk.lm.preprocessing import padded_everygram_pipeline
+from nltk.lm import MLE
+
+text = 'i love chinese food. chinese people love food.'
+
+model = MLE(2)
+
+
+def preprocessText(txt):  # preprocess the sentences text
+    sentences = txt.split('.')
+    if '' in sentences:
+        sentences.remove('')
+    sentences = [sentence.strip(' ') for sentence in sentences]
+
+    return [sentence.split(' ') for sentence in sentences]
+
+
+'''def preprocess(sentence_list):  # preprocess the bigram model
+    preprocessed = [pad_both_ends(s, n=2) for s in sentence_list]
+    tokens = list(flatten(preprocessed))
+    model = bigrams(tokens)
+    return model
+    
+b_t = preprocess(preprocessText(text))
+v = flatten(preprocessText(text))
+training(b_t, v)
+'''
+
+
+# preprocess the bigram model (method 2 more direct, to avoid re-creating the text in memory)
+def preprocess(sentence_list):
+    b_train, vocabu = padded_everygram_pipeline(2, sentence_list)  # 2 for bigram
+    return b_train, vocabu  # return the bigram model
+
+
+def training(t, v):
+    model.fit(t, v)
+
+
+if __name__ == '__main__':
+    b_training, vocabulary = preprocess(preprocessText(text))  # get bigram training and its vocabulary
+
+    training(b_training, vocabulary)  # fit/adjust/improve the model
+    print(model.score('chinese', ['love']))  # get the likelihood of 'love chinese' (bigram model)
+    print(model.score('chinese', ['i', 'love']))  # get the likelihood of 'i love chinese' (trigram model)
+    print(model.score('food', ['i', 'love', 'chinese']))  # get the likelihood of 'i love chinese food' (4-gram model)
+
+    tex = 'japon love'
+    test = list(bigrams(flatten(preprocessText(tex))))  # preprocess test
+    print(test)
+    print(model.perplexity(test))  # perplexity of the model
---
+++ WordPrediction/Next_word_prediction/app.py
@@ -0,0 +1,63 @@
+from nltk.lm.preprocessing import padded_everygram_pipeline
+from nltk.lm import MLE
+
+model = MLE(2)
+
+
+def preparecorpus(file):
+    book = open(file, "r")
+    txt = book.readlines()
+    list_sentences = list([a.translate({ord(i): None for i in '\n,?!:;'}) for a in txt])  # delete ponctuations
+    list_sentences = [elem.split('.') for elem in list_sentences]
+    for elem in list_sentences:
+        if '' in elem:
+            elem.remove('')
+
+    txt = [sentence.strip(' ') for sentences in list_sentences for sentence in sentences]
+    txt = map(str.lower, txt)
+    return [sentence.split(' ') for sentence in txt]
+
+
+def preprocess(sentence_list):
+    b_train, vocabu = padded_everygram_pipeline(2, sentence_list)  # 2 for bigram
+    return b_train, vocabu  # return the bigram model train and vocabulary
+
+
+def training(t, v):
+    model.fit(t, v)
+
+
+if __name__ == '__main__':
+    b_training, vocabulary = preprocess(preparecorpus('corpus'))  # get bigram training and its vocabulary
+
+    training(b_training, vocabulary)  # fit/adjust/improve the model
+
+    sentence = []
+    print("Appuyez 'enter' apres avoir ecrit chaque mot et 'space' lorsque vous avez terminé votre phrase\n"
+          "Veuillez ecrire svp")
+    while True:
+        word = input(' '.join(sentence) + ' ')
+        sentence.append(word)
+        next_word = model.generate(3, text_seed=[word], random_seed=2)  # list of 3 next word guessed
+        if '<s>' in next_word:
+            next_word.remove('<s>')
+
+        if '</s>' in next_word:
+            next_word.remove('</s>')
+
+        print(next_word[0])  # next word
+
+        if word == ' ':
+            break
+
+    print("votre phrase est : " + " ".join(sentence))
+
+
+
+
+
+
+
+
+
+
